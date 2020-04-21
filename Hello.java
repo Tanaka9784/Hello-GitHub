a006de4b002fc55a96bc7f0c191f77e2d965155f
Tanaka9784 <64070381+Tanaka9784@users.noreply.github.com>
2020-04-22 00:20:07 +0900

    Add files via upload
    Add Hello.java

---
+++ Hello.java
@@ -0,0 +1,28 @@
+public class Hello
+{
+	//This is made like singleton because of my plactice. 	
+
+	private static Hello hello;
+	private static String message;
+
+	static{
+		hello = new Hello();
+		message = "Hello world!";
+	}	
+
+	private Hello(){
+		super();
+	}
+
+	public static Hello getInstance(){
+		return hello;
+	}
+
+	public String getMessage(){
+		return this.message;
+	}
+	
+	public static void main(String[] args){
+		System.out.println(getInstance().getMessage());
+	}
+}
