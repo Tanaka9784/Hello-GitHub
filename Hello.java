ffa4be93de380ea23afce58123136b9a154cb7e5
sakura <sakura1@example.com>
2020-04-22 05:28:01 +0100

    Modify message field

--- Hello.java
+++ Hello.java
@@ -1,18 +1,22 @@
-public class Hello
-{
-	//This is made like singleton because of my plactice. 	
 
-	private static Hello hello;
-	private static String message;
+
+//This is made like singleton because of my plactice. 	
+
+public class Hello <T>{
+
+	//The meaning of type parameter T is nothing.
+
+	private static Hello<?> hello;
+	private String message;
 	private String something;
 
 	static{
-		hello = new Hello();
-		message = "Hello world!";
-	}	
+		hello = new Hello<Object>();
+	}
 
 	private Hello(){
 		super();
+		this.message = "Hello world!";
 	}
 
 	public static Hello getInstance(){
@@ -23,6 +27,14 @@ public class Hello
 		return this.message;
 	}
 	
+	public void setMessage(String message){
+		this.message = message;
+	}
+
+	public void setSomething(String something){
+		this.something = something;
+	}
+
 	public static void main(String[] args){
 		System.out.println(getInstance().getMessage());
 	}
