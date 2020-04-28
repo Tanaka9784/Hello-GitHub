e30e29c96e3163dca1a218574ec73cfd90e90a1a
sakura <sakura1@example.com>
2020-04-28 08:25:52 +0100

    modify a little

--- Hello.java
+++ Hello.java
@@ -1,52 +1,30 @@
+/*
+ *
+ * javac 1.8.0_60
+ *
+ *
+ *
+ *
+ *
+ *
+ **/
+public class Hello{
 
-
-//This is made like singleton because of my plactice. 	
-
-public class Hello <T>{
-
-	//The meaning of type parameter T is nothing.
-	
-	public enum Greeting{
-		Hello, GoodMorning, GoodEvening;	
+	enum Greeting{
+		HELLO("Hello"), SPACE(" "), WORLD("World"), EXCLAMATION("!");	
+		private String value;
+		private Greeting(String value){
+			this.value = value;
+		};
+		public String getValue(){
+			return value;
+		}
 	};
-
-	private static Hello<?> hello;
-	private String message;
-	private T something;
-
-	static{
-		hello = new Hello<Object>();
-	}
-
-	private Hello(){
-		super();
-		//this.message = "Hello world!";
-	}
-
-	public static Hello getInstance(){
-		return hello;
-	}
-
-	public String getMessage(){
-		return this.message;
-	}
-	
-
-	public T getSomething(){
-		return this.something;
-	}
-
-	public void setMessage(String message){
-		this.message = message;
-	}
-
-	public void setSomething(T something){
-		this.something = something;
-	}
-
 	public static void main(String[] args){
-		Hello hello = getInstance();
-		hello.setMessage(Greeting.Hello + " World!");
-		System.out.println(hello.getMessage());
+	
+		for(Greeting v: Greeting.values()){
+			System.out.print(v.getValue());
+		}
+		System.out.println();
 	}
 }
